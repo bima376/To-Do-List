b4191e28abdbf31a10c0dd584ddb8b0a18214fc5
bimacahyo <bimacahyo376@gmail.com>
2024-10-12 15:32:44 +0700

    laravel crud

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/todo', [TodoController::class, 'index'])->name('todo');
+Route::post('/todo', [TodoController::class, 'store'])->name('todo.post');
+Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update');
+Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.delete');
