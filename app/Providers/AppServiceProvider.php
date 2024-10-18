45def7f418f927f9a6c2276f9d2f815bcb30d368
bimacahyo <bimacahyo376@gmail.com>
2024-10-18 10:03:45 +0700

    add bootstrap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,12 +12,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        
     }
 
     /**
      * Bootstrap any application services.
-     */
+     */ 
     public function boot(): void
     {
         Paginator::useBootstrapFive();
